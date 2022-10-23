bbb090228018f1c77a5316380131c38f88598c93
unknown <wtung126@gmail.com>
2022-10-23 16:27:12 +0800

    Tung 10 commit

--- client/src/components/button.jsx
+++ client/src/components/button.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from "styled-components"
+
+const ButtonComponent = styled.button`
+    position: relative;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    text-decoration: none;
+    vertical-align: middle;
+    cursor: pointer;
+    user-select: none;
+    border-radius: 0.3rem;
+    padding: 0 ${(props) => 
+      props.size === "sm" ? "1.1rem"
+      : props.size === "md" ? "1.4rem"
+      : props.size === "lg" ? "1.6rem" : "1.1rem"};
+    height: ${(props) => 
+      props.size === "sm" ? "34px"
+      : props.size === "md" ? "37px"
+      : props.size === "lg" ? "40px" : "34px"};
+    font-weight: 500;
+    border: 1px solid #8e00b9;
+  `;
 
 const Button = ({
   disabled,
@@ -7,16 +31,20 @@ const Button = ({
   onClick,
   isLoading,
   className,
-}) => (
-  <button
-    type="button"
-    className={`btn btn-brand ${className || ''}`}
-    disabled={disabled || isLoading}
-    onClick={onClick}
-  >
-    {!isLoading ? children : <span>{`Loading...`}</span>}
-  </button>
-);
+  id,
+  size,
+}) => {
+  return (
+    <ButtonComponent
+      type="button"
+      className={`btn-component ${className || ''}`}
+      disabled={disabled || isLoading}
+      id={id}
+      onClick={onClick}
+    >
+      {!isLoading ? children : <span>{`Loading...`}</span>}
+    </ButtonComponent>
+)};
 
 Button.propTypes = {
   disabled: PropTypes.bool,
@@ -24,6 +52,8 @@ Button.propTypes = {
   onClick: PropTypes.func.isRequired,
   isLoading: PropTypes.bool,
   className: PropTypes.string,
+  id: PropTypes.any,
+  size: PropTypes.string,
 };
 
 export default Button;
---
+++ client/src/form/formikDropDownList.jsx
@@ -0,0 +1,101 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { connect, getIn } from 'formik';
+import Select from 'react-select';
+
+
+const FormikDropDownList = (props) => {
+  const {
+    formik,
+    name,
+    id,
+    label,
+    // disabled,
+    values,
+    onAfterSet,
+    onValueChange,
+    enableDefault,
+    defaulValuetLabel,
+  } = props;
+
+  const error = getIn(formik.errors, name);
+  const touch = getIn(formik.touched, name);
+  const formikValue = getIn(formik.values, name);
+  const isInvalid = error && touch;
+
+
+  const onHandleBlur = () => {
+    formik.setFieldTouched(name, true, true);
+    if (onAfterSet) {
+      setTimeout(() => onAfterSet(), 0);
+    }
+  };
+
+  const onHandleChange = (value) => {
+    formik.setFieldValue(name, value);
+    if (onValueChange) {
+      onValueChange(value);
+    }
+  };
+
+  if (enableDefault) {
+    if (values.length > 0) {
+      if (values[0].label !== 'NONE' && values[0].label !== defaulValuetLabel) {
+        values.splice(0, 0, {
+          label: defaulValuetLabel || 'NONE',
+          value: 0,
+          content: null,
+        });
+      }
+    } else if (values.length === 0) {
+      values.push({
+        label: defaulValuetLabel || 'NONE',
+        value: 0,
+        content: null,
+      });
+    }
+  }
+
+  return (
+    <div>
+        <label htmlFor={id} className={'text-purple'}>
+          {label}
+        </label>
+        <Select
+          id={id}
+          name={name}
+          options={values}
+          multi={true}
+          onChange={value => onHandleChange(value)}
+          onBlur={onHandleBlur}
+          value={formikValue}
+        />
+        {isInvalid && (<div style={{ color: 'red', marginTop: '.5rem' }}>{error}</div>)}
+      </div>
+  );
+};
+
+FormikDropDownList.propTypes = {
+  formik: PropTypes.any,
+  name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  label: PropTypes.oneOfType([PropTypes.element.isRequired, PropTypes.string.isRequired]),
+  disabled: PropTypes.bool,
+  values: PropTypes.array.isRequired,
+  includeDefault: PropTypes.bool,
+  onAfterSet: PropTypes.func,
+  withoutPortal: PropTypes.bool,
+  placeholder: PropTypes.string,
+  onValueChange: PropTypes.func,
+  hasMarginBottom: PropTypes.bool,
+  hasPadding: PropTypes.bool,
+  enableDefault: PropTypes.bool,
+  isBgWhite: PropTypes.bool,
+  textPurple: PropTypes.bool,
+  containerStyle: PropTypes.any,
+  controlContainer: PropTypes.any,
+  defaulValuetLabel: PropTypes.string,
+  isRequired: PropTypes.bool,
+};
+
+export default connect(FormikDropDownList);
